026c782a6fe6faa2537dfcfc7727e273cafe2df7
BCNITB <97254492+BCNITB@users.noreply.github.com>
2023-05-27 13:07:15 +0200

    Interface to control the schema of records at databas created + changes at form

---
+++ src/app/interfaces/items.ts
@@ -0,0 +1,9 @@
+export interface Items {
+    id?:            string;
+    category:       string;
+    item:           string;
+    clasification:  string;
+    consult:        string;
+    answer:         string;
+    historic:       boolean;
+}
