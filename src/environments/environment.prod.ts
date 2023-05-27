148d188b936a75fbee935c55b96629474a1e7ae5
BCNITB <97254492+BCNITB@users.noreply.github.com>
2023-05-27 07:59:21 +0200

    Link with firestore database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'tiflo-web',
+    appId: '1:1066166367070:web:38b23912714b89280d95cf',
+    storageBucket: 'tiflo-web.appspot.com',
+    apiKey: 'AIzaSyDgn-Le7GojAzbUYpCsZADj2Rw9A2z5ymw',
+    authDomain: 'tiflo-web.firebaseapp.com',
+    messagingSenderId: '1066166367070',
+    measurementId: 'G-G5YLMPVQ9S',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'tiflo-web',
+    appId: '1:1066166367070:web:38b23912714b89280d95cf',
+    storageBucket: 'tiflo-web.appspot.com',
+    apiKey: 'AIzaSyDgn-Le7GojAzbUYpCsZADj2Rw9A2z5ymw',
+    authDomain: 'tiflo-web.firebaseapp.com',
+    messagingSenderId: '1066166367070',
+    measurementId: 'G-G5YLMPVQ9S',
+  },
   production: false
 };
 
