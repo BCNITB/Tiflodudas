98c41b4673754bc47da65b318f79f02c9b38dbb8
BCNITB <97254492+BCNITB@users.noreply.github.com>
2023-05-28 20:49:45 +0200

    All buttons checked and minor bug fixed

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ItemService } from 'src/app/services/item.service';
 import { Items } from 'src/app/interfaces/items';
-import { NavController } from '@ionic/angular';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-details',
@@ -13,14 +13,17 @@ export class DetailsPage implements OnInit {
 
   items:  Items[];
   cat:    string;
+  logged: string;
 
   constructor(
     private route: ActivatedRoute,
     private itemsSrvc: ItemService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private alertCtrl: AlertController
   ) {
     this.items =[];
     this.cat = String(this.route.snapshot.paramMap.get('id'));
+    this.logged = "";
    }
 
   ngOnInit() {
@@ -29,11 +32,39 @@ export class DetailsPage implements OnInit {
     });
   }
 
+  ionViewWillEnter(){
+    this.logged = String(localStorage.getItem('state'));
+  }
+
   sendSol(id: any){
 
   }
 
   goBack(){
     this.navCtrl.back();
+  }
+
+  edit(id: any){
+
+  }
+
+  async erase(item: Items){
+    const alert = await this.alertCtrl.create({
+      header: 'Eliminar registro',
+      message: '¿Quieres elimnar este registro?',
+      buttons: [
+        {
+          text: 'No',
+        },
+        {
+          text: 'Sí',
+          handler: () => {
+            this.itemsSrvc.deleteItem(item);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
