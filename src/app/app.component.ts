ba2a3fa98b8cf26bee981daeae67311d5420ece5
BCNITB <nosvaudi@gmail.com>
2025-10-01 19:10:08 +0200

    Bug fixed. Now all tabs are allowed in all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Platform } from '@ionic/angular';
 
 @Component({
@@ -10,14 +10,28 @@ export class AppComponent {
   
   logged: string;
   isMobile: boolean;
+  isAdmin: boolean = false; // TODO: Implement logic to check if the user is an admin
+  showTabs: boolean;
 
   constructor(private platform: Platform) {
     this.initializeApp();
+    this.checkPlatform();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.isMobile = this.platform.is('mobile');
     });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkPlatform();
+  }
+
+  private checkPlatform() {
+    const isApp = this.platform.is('hybrid');
+    const isMobileWeb = this.platform.width() < 768 && !isApp;
+    this.showTabs = isApp || isMobileWeb;
   }
 }
