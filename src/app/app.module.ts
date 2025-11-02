4fe9f431f7561518866fcb61dfadac5ab8d74ba7
Cambronero <ing.geol@gmail.com>
2025-11-02 14:41:54 +0100

    Implement PWA and Gemini integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,7 +18,14 @@ import { TabsPageModule } from './pages/tabs/tabs.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PipesModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore()), provideAuth(() => getAuth()), TabsPageModule, HttpClientModule],
+  imports: [BrowserModule, PipesModule, IonicModule.forRoot(), 
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }), provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore()), provideAuth(() => getAuth()), TabsPageModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
